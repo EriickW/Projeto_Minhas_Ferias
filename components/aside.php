c0ae162d260503d1c2eab889fb6a4795209ce10a
Erick Willian <87780768+EriickW@users.noreply.github.com>
2022-09-24 21:39:29 -0300

    Arrumando a area do professor

--- components/aside.php
+++ components/aside.php
@@ -1,19 +1,28 @@
-<aside class="active">
-        <div class="aside-content">
-            <nav>
-                <ul>
-                    <li class="ativa"><ion-icon name="home" class="icon"></ion-icon><a href="../pages/areaProfessor.php" >Página inicial</a></li>
-                    <li><ion-icon name="document-text"  class="icon"></ion-icon><a href="../pages/redacoes.php">Redações</a></li>
-                    <li><ion-icon name="call" class="icon"></ion-icon><a href=""> Contatos</a></li>
-                    <li><ion-icon name="settings" class="icon"></ion-icon><a href="">Configurações</a></li>
-                    <li id="trocarTema"><ion-icon name="invert-mode" class="icon"></ion-icon>Trocar Tema</li>
-                    <li><ion-icon name="document-text"  class="icon"></ion-icon><a href="../index.php">Fazer Redação</a></li>
-                </ul>
-            </nav>
 
-            <a href="../pages/logout.php" id="sair"><ion-icon name="exit" class="icon"></ion-icon>Sair</a>
+
+<aside class="g-col-4 text-center" style="height: 100vh;width:500px;background:black;">
+
+
+    <section class="d-flex flex-column justify-content-center align-items-center" style="width:100%;">
+        <div style="background:gray;width:100px;height:100px;" ></div>
+        <h4><?php
+            echo $_SESSION['nome'];
+        ?></h4>        
+    </section>
+            <section style="width:100%;padding:0;">
+                <nav>
+                    <ul classs="list-group "style="padding:0;">
+                        <li class="list-group-item  p-2"><a href="../pages/areaProfessor.php" >Página inicial</a></li>
+                        <li class="list-group-item  p-2"><a href="../pages/redacoes.php">Redações</a></li>
+                        <li class="list-group-item  p-2"><a href="../pages/contatos.php"> Contatos</a></li>
+                        <li class="list-group-item  p-2"><a href="../pages/config.php">Configurações</a></li>
+                        <li class="list-group-item  p-2" id="trocarTema">Trocar Tema</li>
+                        <li class="list-group-item  p-2"><a href="../index.php">Fazer Redação</a></li>
+                    </ul>
+                </nav>
+                <a class="text-center"href="../pages/logout.php" id="sair">Sair</a>
+            </section>
             
-        </div>
 
         
 </aside>
--- pages/areaProfessor.php
+++ pages/areaProfessor.php
@@ -11,41 +11,39 @@ include('../src/db/protect.php');
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../src/styles/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous" defer></script>
     <script type="module"src="../src/scripts/main.js" defer></script>
     <title>Area do Professor</title>
 </head>
-<body id="areaProfessor"class="light-theme  ">
+<body id="areaProfessor"class="light-theme d-grid d-flex">
 
-    <!-- ========================== NAV ======================== -->
-    <?php
-    include("../components/navAreaProf.php")
-    ?>
+
 
 
     <!-- ========================== ASIDE ======================== -->
     <?php
-    include("../components/aside.php")
+    include("../components/aside.php");
     ?>
 
-    <main id="main-area-professor">  
-        <section class="main-tutorial-content">
+    <main id="main-area-professor container g-col-8 text-center m-2">  
+        <section class="main-tutorial-content grid">
+            <h2 class="text-center">Tutorial da Plataforma</h2>
+           <div class="  d-flex grid">
        
-           <div class="tutorial">
-           <h2 >Tutorial da Plataforma</h2>
-                <div class="box">
+                <div class="g-col-6 g-col-md-4">
                     <h4>Aba de redações</h4>
                     <div class="text-hover">
                         <p>Após logar em sua conta você terá que clicar na aba de redações e procurar o dia em que você aplicou a redação...</p>
                     </div>
                 </div>
-                <div class="box box0" >
+                <div class="g-col-6 g-col-md-4" >
                     <h4>Encontrando redações</h4>
                 <div class="text-hover">
                     <p>Após encontrar a data basta apertar, ele funciona como uma pasta que guarda todas as redações feitas naquela data especifica, após clicar irá aparecer as redações e basta escolher uma e corrigir</p>
                 </div> 
             </div>
-                <div class="box box1">
+                <div class="g-col-6 g-col-md-4">
                     <h4>Corrigindo</h4>
                     <div class="text-hover">
                         <p>Após ter escolhido a redação ao lado da imagem da redação feita pelo aluno terá um espaço escrito "Atribuir Nota" ali você irá dar a nota de 0 a 10</p>
