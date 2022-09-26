6f2ee61c419c43c2777e555cc15ea69a0e27546c
Erick Willian <87780768+EriickW@users.noreply.github.com>
2022-09-25 21:33:33 -0300

    Integrando o bootstrap na area do professsor

--- components/aside.php
+++ components/aside.php
@@ -1,28 +1,31 @@
 
 
-<aside class="g-col-4 text-center" style="height: 100vh;width:500px;background:black;">
-
-
-    <section class="d-flex flex-column justify-content-center align-items-center" style="width:100%;">
-        <div style="background:gray;width:100px;height:100px;" ></div>
-        <h4><?php
-            echo $_SESSION['nome'];
-        ?></h4>        
-    </section>
-            <section style="width:100%;padding:0;">
-                <nav>
-                    <ul classs="list-group "style="padding:0;">
-                        <li class="list-group-item  p-2"><a href="../pages/areaProfessor.php" >Página inicial</a></li>
-                        <li class="list-group-item  p-2"><a href="../pages/redacoes.php">Redações</a></li>
-                        <li class="list-group-item  p-2"><a href="../pages/contatos.php"> Contatos</a></li>
-                        <li class="list-group-item  p-2"><a href="../pages/config.php">Configurações</a></li>
-                        <li class="list-group-item  p-2" id="trocarTema">Trocar Tema</li>
-                        <li class="list-group-item  p-2"><a href="../index.php">Fazer Redação</a></li>
-                    </ul>
-                </nav>
-                <a class="text-center"href="../pages/logout.php" id="sair">Sair</a>
-            </section>
-            
-
+<aside id="aside"class="position-relative g-col-4 text-center flex-shrink-0" style="height: 100%;width:150px;">
+   
+    <section id="aside-content"class="position-fixed">
         
+        <div class="d-flex flex-column justify-content-center align-items-center mt-5" style="width:100%;">
+            <div  class="mb-3 rounded-circle"style="background:gray;width:90px;height:90px;" ></div>
+            <h5><?php
+                echo $_SESSION['nome'];
+            ?></h5>
+        </div>
+        <div class="mt-4 text-center "style="width:100%;height:100%;">
+            <nav style="height: 100%;width:150px;">
+                <ul classs="list-group "style="padding:0;">
+                    <li class="list-group-item  p-2"><a href="../pages/areaProfessor.php" >Página inicial</a></li>
+                    <li class="list-group-item  p-2"><a href="../pages/redacoes.php">Redações</a></li>
+                    <li class="list-group-item  p-2"><a href="../pages/contatos.php"> Contatos</a></li>
+                    <li class="list-group-item  p-2"><a href="../pages/config.php">Configurações</a></li>
+                    <li class="list-group-item  p-2" id="trocarTema">Trocar Tema</li>
+                    <li class="list-group-item  p-2"><a href="../index.php">Fazer Redação</a></li>
+                    <a class="list-group-item "href="../pages/logout.php" id="sair">Sair</a>
+                </ul>
+            </nav>
+            
+        </div>
+        
+    </section>
+
+    
 </aside>
--- pages/areaProfessor.php
+++ pages/areaProfessor.php
@@ -10,13 +10,15 @@ include('../src/db/protect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,500&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="../src/styles/professor.css">
     <script type="module"src="../src/scripts/main.js" defer></script>
     <title>Area do Professor</title>
 </head>
-<body id="areaProfessor"class="light-theme d-grid d-flex">
+<body id="areaProfessor"class="light d-grid d-flex  gap-3">
 
 
 
@@ -26,26 +28,28 @@ include('../src/db/protect.php');
     include("../components/aside.php");
     ?>
 
-    <main id="main-area-professor container g-col-8 text-center m-2">  
-        <section class="main-tutorial-content grid">
-            <h2 class="text-center">Tutorial da Plataforma</h2>
-           <div class="  d-flex grid">
+
+
+    <main id=" g-col-8 text-center position-relative ">  
+        <section class="mt-5 grid text">
+            <h1 class=" mb-5">Tutorial da Plataforma</h1>
+           <div class=" flex-wrap flex-md-nowrap d-flex grid  text-justify gap-4">
        
                 <div class="g-col-6 g-col-md-4">
                     <h4>Aba de redações</h4>
-                    <div class="text-hover">
+                    <div class="">
                         <p>Após logar em sua conta você terá que clicar na aba de redações e procurar o dia em que você aplicou a redação...</p>
                     </div>
                 </div>
                 <div class="g-col-6 g-col-md-4" >
                     <h4>Encontrando redações</h4>
-                <div class="text-hover">
+                <div class="">
                     <p>Após encontrar a data basta apertar, ele funciona como uma pasta que guarda todas as redações feitas naquela data especifica, após clicar irá aparecer as redações e basta escolher uma e corrigir</p>
                 </div> 
             </div>
                 <div class="g-col-6 g-col-md-4">
                     <h4>Corrigindo</h4>
-                    <div class="text-hover">
+                    <div class="">
                         <p>Após ter escolhido a redação ao lado da imagem da redação feita pelo aluno terá um espaço escrito "Atribuir Nota" ali você irá dar a nota de 0 a 10</p>
                     </div>
                 </div>
@@ -53,12 +57,14 @@ include('../src/db/protect.php');
            </div>
 
         </section>
-        <section class="aplicacao">
+        <section class="text grid">
             <h2>Como aplicar a Redação?</h2>
-            <div>
+            <div class="g-col-12">
                 <p>Para aplicar esse tipo de redação você professor precsia explicar como vai funcionar para que as crianças entemdam corretamente como fazer caso elas tenham duvidas</p>
             </div>
+           
         </section>
+      
     </main>
 
 
@@ -66,3 +72,4 @@ include('../src/db/protect.php');
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
+
